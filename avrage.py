d3e166e3d0b8051152d12c2b87fdb2ae87dc6765
subodhstha <54183791+subodhstha@users.noreply.github.com>
2021-05-27 10:42:58 +0545

    Add files via upload

---
+++ avrage.py
@@ -0,0 +1,8 @@
+def main():
+    print("This progaram is to calculate the average of number.")
+    num1 = float(input("Enter the first number "))
+    num2 = float(input("Enter the second number "))
+    average = (num1 + num2)/2
+    print("The average is",average)
+if __name__ == '__main__':
+    main()
---
+++ first.py
@@ -0,0 +1,4 @@
+def main():
+    print("hello Sangam Chor")
+if __name__ == '__main__':
+    main()
