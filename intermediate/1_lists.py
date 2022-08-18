0e1f523e560751a4e8e70a6a9a8c290740d1d194
Subodh Shrestha <shresthasubodh73@gmail.com>
2022-08-18 07:36:45 +0545

    intermediatr

---
+++ intermediate/1_lists.py
@@ -0,0 +1,74 @@
+# myList = ["banana", "cherry", "apple"]
+# print(myList)
+
+# item = myList[1]
+# print(item)
+# print(myList[-1])
+# myList2 = [5, True, "apple", "apple"]
+# print(myList2)
+
+# for i in myList:
+#     print(i)
+
+# if "banana" in myList:
+#     print("yes")
+# else:
+#     print("no")
+
+# print(len(myList))
+
+# myList.append("lemon")
+
+# print(len(myList))
+# print(myList)
+
+# myList.insert(1, "blackberry")
+# print(myList)
+
+# myList.pop()
+# print(myList)
+
+# myList.remove("banana")
+# print(myList)
+
+# myList.clear()
+# print(myList)
+
+# myList.reverse()
+# print(myList)
+
+# myList3 = [5, 58, -8, 6, 5]
+# print(myList3)
+# newList = sorted(myList3)
+# print(newList)
+
+# myList4 = [0] * 4
+# print(myList4)
+
+# myList5 = [1, 2, 3, 4, 5, 6]
+# newList = myList5 + myList4
+# print(newList) 
+
+# myList6 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# a = myList6[1:7]
+# a = myList6[5::]
+# a = myList6[::2]
+# a = myList6[::6]
+# a = myList6[::-1]
+# print(a)
+
+# my_list7 = ["banana", "cherry", "apple"]
+# list_cpy = my_list7
+# list_cpy.append("mango")
+# print(list_cpy)
+# print(my_list7)
+# list_cpy2 = my_list7.copy()
+# list_cpy2.append("berry")
+# print(my_list7)
+# print(list_cpy)
+# print(list_cpy2)
+
+myList8 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+myList9 = [i * i for i in myList8]
+print(myList8)
+print(myList9)
---
+++ intermediate/2_tuples.py
@@ -0,0 +1,19 @@
+# tuple = ("max", 28, "Boston")
+# tuple = "max", 28, "Boston"
+
+# tuple = "max",
+# print(type(tuple))
+
+tuple = ("max", 28, "Boston")
+# print(tuple)
+
+# item = tuple[1]
+# print(item)
+
+# for i in tuple:
+#     print(i)
+
+if "yes" in tuple:
+    print("yes")
+else:
+    print("no")
