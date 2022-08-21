f67b1e7dd62978533aa48cfe179066713f85bff5
subodhstha <shresthasubodh73@gmail.com>
2022-08-21 22:46:15 +0545

    py

--- intermediate/2_tuples.py
+++ intermediate/2_tuples.py
@@ -4,7 +4,7 @@
 # tuple = "max",
 # print(type(tuple))
 
-tuple = ("max", 28, "Boston")
+# tuple = ("max", 28, "Boston")
 # print(tuple)
 
 # item = tuple[1]
@@ -13,7 +13,54 @@ tuple = ("max", 28, "Boston")
 # for i in tuple:
 #     print(i)
 
-if "yes" in tuple:
-    print("yes")
-else:
-    print("no")
+# if "yes" in tuple:
+#     print("yes")
+# else:
+#     print("no")
+
+# tuple2 = ("a", "b", "c", "d")
+# print(len(tuple2))
+# print(tuple2.count("o"))
+# print(tuple2.count("a"))
+
+# print(tuple2.index("a"))
+# list = list(tuple2)
+# print(list)
+
+# tuple3 = tuple(list)
+# print(tuple3)
+
+# a = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+# b = a[2:]
+# b = a[::2]
+# b = a[::-1]
+
+# print(b)
+
+
+# tuple4 = "max", 28, "boston"
+
+# name, age, city = tuple4
+# print(name)
+# print(age)
+# print(city)
+
+# tuple5 = (0, 1, 2, 3, 4, 5)
+
+# i1, *i2, i3 = tuple5
+# print(i1)
+# print(i3)
+# print(i2)
+
+# list occupied more space than tuple
+# import sys
+# list = [0, 1, 2, "hello", True]
+# tuple6 = (0, 1, 2, "hello", True)
+# print(sys.getsizeof(list), "bytes")
+# print(sys.getsizeof(tuple6), "bytes")
+
+# tuple is faster than list
+import timeit
+print(timeit.timeit(stmt="[0, 1, 2, 3, 4, 5]",number=1000000))
+print(timeit.timeit(stmt="(0, 1, 2, 3, 4, 5)",number=1000000))
---
+++ intermediate/3_dictionaries.py
@@ -0,0 +1,62 @@
+dict1 = {"name":"Max", "age" : 28, "city" : "New York"}
+# print(dict1)
+
+# dict2 = dict(name = "Mary", age = 27, city = "Boston")
+# print(dict2)
+
+# value = dict2["age"]
+# print(value)
+
+# dict1["email"] = "max@abc.com"
+# print(dict1)
+
+# dict1["email"] = "qwe@abc.com"
+# print(dict1)
+
+
+# del dict1["name"]
+# dict1.popitem()
+# print(dict1)
+
+# if "name" in dict1:
+#     print(dict1["name"])
+
+# try:
+#     print(dict1["name"])
+# except:
+#     print("Error")
+
+# for key in dict1:
+#     print(key)
+
+
+# for key in dict1.keys():
+#     print(key)
+
+# for value in dict1.values():
+#     print(value)
+
+# for key, value in dict1.items():
+#     print(key, value)
+
+# dict1_cpy = dict(dict1)
+# print(dict1_cpy)
+
+# dict1_cpy["Email"] = "abc@xyz.com"
+# print(dict1_cpy)
+# print(dict1)
+
+# dict1 = {"name":"Max", "age" : 28, "email": "abc@xyz.com"}
+# dict2 = dict(name = "Mary", age = 22, city = "Boston")
+# dict1.update(dict2)
+# print(dict1)
+
+dict3 = {3:9, 6:36, 9:81}
+print(dict3)
+
+# value = dict3[3]
+# print(value)
+
+tuple1 = (8, 7)
+dict4 = {tuple1: 15}
+print(tuple1)
